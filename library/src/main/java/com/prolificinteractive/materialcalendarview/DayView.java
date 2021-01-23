10fe6047368bb371a367d33fad6771bcf000008d
jqbkeep <jqbkeep@163.com>
2021-01-24 00:34:25 +0800

    提交

--- library/src/main/java/com/prolificinteractive/materialcalendarview/DayView.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/DayView.java
@@ -120,6 +120,7 @@ import static com.prolificinteractive.materialcalendarview.MaterialCalendarView.
     if(date.getDay()== Calendar.getInstance().get(Calendar.DAY_OF_MONTH)){
       setTextColor(getTextColors().getColorForState(
               new int[] { -android.R.attr.state_enabled }, Color.BLACK));
+      setTextSize(17f);
       return "今";
     }else{
       return formatter.format(date);
